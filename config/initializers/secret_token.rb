415cb630381f8764205cf09b13681f40f42438c0
Goodwin Ogbuehi <github@gogbuehi.com>
2014-04-02 16:26:09 +0100

    Initial commit of the app  - Just an empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataProcessor::Application.config.secret_key_base = '448485044f05dc4b18fc8dbbdf1d67b33fc15e84cadd3e91c96f1ac431df4bbf0c5f5fb8c35f0a8b5eb9881fd1e46d2d0ad725a937b6501926a4ca67001fa968'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DataProcessor::Application.config.session_store :cookie_store, key: '_data_processor_session'
